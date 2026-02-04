5cbf84b3c0fc119cbe1da233895a4957fc0e26c6
Johannes <johannes.programming@gmail.com>
2026-02-04 15:57:43 +0100

    Version 1.0.5

--- src/raisefunction/core/__init__.py
+++ src/raisefunction/core/__init__.py
@@ -1,29 +1,27 @@
 from typing import *
 
-from overloadable import overloadable
-
 __all__ = ["raisefunction"]
 
-
-@overloadable
-def raisefunction(*args: Any, **kwargs: Any) -> int:
-    "This function works as dispatcher."
-    argc: int = len(args)
-    keys: set = set(kwargs.keys())
-    if argc <= 1 and keys == set():
-        return 1
-    if argc == 0 and keys == {"exc"}:
-        return 1
-    return 2
+DEFAULT = object()
 
 
-@raisefunction.overload(1)
-def raisefunction(exc: BaseException) -> None:
+@overload
+def raisefunction(
+    exc: BaseException | type[BaseException],
+) -> Never: ...
+@overload
+def raisefunction(
+    exc: BaseException | type[BaseException],
+    cause: Optional[BaseException],
+) -> Never: ...
+
+
+def raisefunction(
+    exc: BaseException | type[BaseException],
+    cause: Optional[BaseException] | object = DEFAULT,
+) -> Never:
     "This function raises the given exception."
-    raise exc
-
-
-@raisefunction.overload(2)
-def raisefunction(exc: BaseException, cause: Optional[BaseException]) -> None:
-    "This function raises the given exception with the given cause."
-    raise exc from cause
+    if cause is DEFAULT:
+        raise exc
+    else:
+        raise exc from cause
--- src/raisefunction/tests/__init__.py
+++ src/raisefunction/tests/__init__.py
@@ -5,6 +5,10 @@ __all__ = ["test"]
 
 def test() -> unittest.TextTestRunner:
     "This function runs all the tests."
+    loader: unittest.TestLoader
+    tests: unittest.TestSuite
+    runner: unittest.TextTestRunner
+    result: unittest.TextTestResult
     loader = unittest.TestLoader()
     tests = loader.discover(start_dir="raisefunction.tests")
     runner = unittest.TextTestRunner()
